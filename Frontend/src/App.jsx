9e0428114ec53497905c29dfe93f9040c901c4f6
manthan <manthanp1739@gmail.com>
2025-12-23 12:20:05 +0530

    njnj

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
-import Courses from "./pages/Courses";
-import CreateCourse from "./pages/CreateCourse";
-import UploadCourse from "./pages/UploadCourse";
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/create-course" element={<CreateCourse />} />
-        <Route path="/uploads" element={<UploadCourse />} />
-      </Routes>
+     <div>hello</div>
     </>
   );
 }
